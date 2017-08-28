4ef934ae7fb1b8d55fb7b5eb2fc2891162d026a4
Kanishk Singh <singh.kanishk07@gmail.com>
2017-08-29 02:05:50 +0530

    Added comments

--- Notification_Processor.py
+++ Notification_Processor.py
@@ -14,21 +14,21 @@ def line_no(fname):
     return i + 1
 
 def FileBot():
-    with open("SeriesName.txt", 'r') as sr:
+    with open("SeriesName.txt", 'r') as sr: #Use absolute paths
         seriesTitle = sr.read().strip('\n')
-    with open("EpisodeDet.txt", 'r+') as file:
+    with open("EpisodeDet.txt", 'r+') as file: #Use absolute paths
         episodeNames = file.read()
 
     #Send message depending upon number of files processed (checked with number of lines)
-    if line_no("EpisodeDet.txt") > 2:
+    if line_no("EpisodeDet.txt") > 2: #Use absolute paths
         telegraph_submit(seriesTitle, episodeNames)
     else:
         seriesTitle = '<b>' + seriesTitle + '</b>'
         telegram(seriesTitle, episodeNames)
     #Empty File contents
-    with open("SeriesName.txt", 'w') as sr:
+    with open("SeriesName.txt", 'w') as sr: #Use absolute paths
         pass
-    with open("EpisodeDet.txt", 'w') as file:
+    with open("EpisodeDet.txt", 'w') as file: #Use absolute paths
         pass
 
 if __name__ == '__main__':
