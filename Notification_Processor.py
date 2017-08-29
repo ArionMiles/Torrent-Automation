2f7aa1bdd5e8f32b08c28d93b79c5e9effdedab1
Kanishk Singh <singh.kanishk07@gmail.com>
2017-08-29 12:55:18 +0530

    Added logging.

--- Notification_Processor.py
+++ Notification_Processor.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
+import logging
 from telegraph_post import telegraph_submit
 from telegram import telegram
+
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
 
 __author__ = 'Kanishk Singh (Arion Miles)'
 __license__ = "MIT"
@@ -16,16 +24,23 @@ def line_no(fname):
 def FileBot():
     with open("SeriesName.txt", 'r') as sr: #Use absolute paths
         seriesTitle = sr.read().strip('\n')
-    with open("EpisodeDet.txt", 'r+') as file: #Use absolute paths
+        logger.debug('Reading %s' % sr)
+    with open("EpisodeDet.txt", 'r') as file: #Use absolute paths
         episodeNames = file.read()
+        logger.debug('Reading %s' % file)
 
     #Send message depending upon number of files processed (checked with number of lines)
+    logger.debug('Checking number of files processed...')
     if line_no("EpisodeDet.txt") > 2: #Use absolute paths
+        logger.debug('More than one file processed. Sending to Telegra.ph')
         telegraph_submit(seriesTitle, episodeNames)
     else:
+        logger.debug('One file processed. Sending directly.')
         seriesTitle = '<b>' + seriesTitle + '</b>'
         telegram(seriesTitle, episodeNames)
+
     #Empty File contents
+    logger.debug('Clearing contents of the text files.')
     with open("SeriesName.txt", 'w') as sr: #Use absolute paths
         pass
     with open("EpisodeDet.txt", 'w') as file: #Use absolute paths
