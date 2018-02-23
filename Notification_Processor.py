6d7f0a3cb67dfe357c751dafbb0f1dcafa406232
Kanishk Singh <singh.kanishk07@gmail.com>
2018-02-23 15:52:18 +0530

    Moved from Python2 to Python3
    Python2 is no longer supported.

--- Notification_Processor.py
+++ Notification_Processor.py
@@ -1,10 +1,11 @@
+#! python3
 # -*- coding: utf-8 -*-
 import logging
 from telegraph_post import telegraph_submit
 from telegram import telegram
 
 logger = logging.getLogger()
-handler = logging.StreamHandler()
+handler = logging.FileHandler('E:/New/config/notifications.log') #Use absolute paths
 formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
@@ -16,34 +17,34 @@ __license__ = "MIT"
 def line_no(fname):
     '''Count number of lines in a text file.'''
     i =- 1
-    with open(fname) as f:
+    with open(fname, encoding='utf-8-sig') as f:
         for i, l in enumerate(f):
             pass
     return i + 1
 
 def FileBot():
-    with open("SeriesName.txt", 'r') as sr: #Use absolute paths
+    with open("E:/New/config/SeriesName.txt", 'r', encoding='utf-8-sig') as sr: #Use absolute paths
         seriesTitle = sr.read().strip('\n')
         logger.debug('Reading %s' % sr)
-    with open("EpisodeDet.txt", 'r') as file: #Use absolute paths
+    with open("E:/New/config/EpisodeDet.txt", 'r', encoding='utf-8-sig') as file: #Use absolute paths
         episodeNames = file.read()
         logger.debug('Reading %s' % file)
 
     #Send message depending upon number of files processed (checked with number of lines)
     logger.debug('Checking number of files processed...')
-    if line_no("EpisodeDet.txt") > 2: #Use absolute paths
-        logger.debug('More than one file processed. Sending to Telegra.ph')
+    if line_no("E:/New/config/EpisodeDet.txt") > 2: #Use absolute paths
+        logger.debug('More than one file processed. Sending to Telegra.ph \n \n')
         telegraph_submit(seriesTitle, episodeNames)
     else:
-        logger.debug('One file processed. Sending directly.')
+        logger.debug('One file processed. Sending directly. \n \n')
         seriesTitle = '<b>' + seriesTitle + '</b>'
         telegram(seriesTitle, episodeNames)
 
     #Empty File contents
     logger.debug('Clearing contents of the text files.')
-    with open("SeriesName.txt", 'w') as sr: #Use absolute paths
+    with open("E:/New/config/SeriesName.txt", 'w', encoding='utf-8-sig') as sr: #Use absolute paths
         pass
-    with open("EpisodeDet.txt", 'w') as file: #Use absolute paths
+    with open("E:/New/config/EpisodeDet.txt", 'w', encoding='utf-8-sig') as file: #Use absolute paths
         pass
 
 if __name__ == '__main__':
